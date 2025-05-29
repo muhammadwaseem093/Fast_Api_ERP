97c43a6b1e29c2b178ae99cdd1c1deadfe82b188
Muhammad Waseem <muhammadwaseem093@gmail.com>
2025-05-29 11:13:22 +0500

    little bit modificaton in users models

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from app.api import api_router
 from app.database import Base, engine 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
 app =FastAPI(title="RBAC API")
 
+origin = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origin,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
